11d591b394c22b1ab04e5a513f1953a2544184c5
Aviq Nurdiansyah Putra <aviq.np@xooey.id>
2024-09-24 18:16:47 +0700

    New

---
+++ Node/client2-cam.py
@@ -0,0 +1,23 @@
+import zmq 
+import cv2
+import time
+import numpy as np
+context = zmq.Context()
+image_socket = context.socket(zmq.PUB)
+image_socket.connect("tcp://127.0.0.1:5004")
+cap = cv2.VideoCapture(0)
+running = True
+def shutdown(signum, frame):
+    global running
+    running = False
+import signal
+signal.signal(signal.SIGINT, shutdown)
+while running:
+    ret, frame = cap.read()
+    _, buffer = cv2.imencode('.jpg', frame)
+    image_bytes = buffer.tobytes()
+    image_socket.send(image_bytes)
+    print("Image sent")
+    time.sleep(2)
+cap.release()
+print("Camera released")
---
+++ Node/client2-dht.py
@@ -0,0 +1,39 @@
+import zmq
+import json
+import time
+import Adafruit_DHT
+import numpy as np
+import random
+
+def randomVoltage():
+    return round(random.uniform(0,10),1)
+def randomCurrent():
+    return round(random.uniform(0,10),1)
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.connect("tcp://127.0.0.1:5004")
+    try:
+        while True:
+            sensor = Adafruit_DHT.DHT22
+            pin = 4
+            tegangan = randomVoltage()
+            arus = randomCurrent()
+            #client_id = "client1"
+            kelembapan, suhu = Adafruit_DHT.read_retry(sensor, pin)
+            if kelembapan is not None and suhu is not None and suhu > 15 and kelembapan < 100:
+                message = f"{suhu:.1f},{kelembapan:.1f},{tegangan},{arus}"
+                socket.send_string(message)
+                print("Data sent - Temperature:", suhu, "Humidity:", kelembapan, "volt:", tegangan, "arus:", arus)
+            else:
+                print("Failed")
+	    
+            time.sleep(1)
+    except KeyboardInterrupt:
+
+        print("Close Client 1")
+if __name__ == "__main__":
+    main()
+    
+
